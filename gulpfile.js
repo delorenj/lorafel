84140d2cdcebe470f77517512ba92528f568c38a
Jarad DeLorenzo <jarad@splashthat.com>
2016-03-26 12:54:07 -0400

    Refactored all sprites. Made gulp file to auto pack and copy them. Removed individual images in favor of a single spritesheet

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,30 @@
 var gulp = require('gulp');
-var glue = require("gulp-sprite-glue");
+var glue = require("gulp-glue");
+var rename = require("gulp-rename");
 
-var iphonehd5 = function() {
-    gulp.src("./joeman/*")
-        .pipe(
-            glue("./Resources/iphonehd5/*.png",{
-                cocos2d: true
-            })
-        )
+gulp.task('sprites:pack', function() {
+    return gulp
+        .src(['/Users/delorenj/cc/Lorafel/sprites/raw/**/*.*'])
+        .pipe(glue({
+            'source': '/Users/delorenj/cc/Lorafel/sprites/raw',
+            'output': '/Users/delorenj/cc/Lorafel/sprites/sheets',                      // required
+            'project': true,
+            'retina': false,
+            'cocos2d': true
+        }))
     ;
-};
-
-gulp.task('sprites:iphonehd5', function() {
-    return iphonehd5();
 });
 
-gulp.task('default', ['sprites:iphonehd5']);
+gulp.task('sprites:copy', function() {
+   return gulp
+       .src('/Users/delorenj/cc/Lorafel/sprites/sheets/*')
+       .pipe(rename(function(path){
+           var name = path.basename;
+           path.dirname = name;
+           path.basename = "sprites";
+       }))
+       .pipe(gulp.dest("/Users/delorenj/code/Lorafel/Resources"))
+    ;
+});
+
+gulp.task('default', ['sprites:pack', 'sprites:copy']);
