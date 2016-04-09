0d11a11f2e60c5b040ab92e44e6da676ddf4fa8d
Jarad DeLorenzo <jarad@splashthat.com>
2016-04-09 11:53:57 -0400

    Implemented sweet match5 'splosions

--- Classes/Match5.cpp
+++ Classes/Match5.cpp
@@ -8,8 +8,25 @@
 using namespace lorafel;
 
 void Match5::run() {
-  auto p = cocos2d::ParticleMeteor::create();
+  auto p = cocos2d::ParticleSystemQuad::create("lineplosion.plist");
   p->setPosition(m_pPrimaryTile->getGrid()->convertToNodeSpace(getTileSetCenter()));
+  p->setAutoRemoveOnFinish(true);
   m_pPrimaryTile->getGrid()->addChild(p, LayerOrder::PARTICLES);
+
+  auto s = cocos2d::ParticleSystemQuad::create("sparkplosion.plist");
+  s->setPosition(m_pPrimaryTile->getGrid()->convertToNodeSpace(getTileSetCenter()));
+  s->setAutoRemoveOnFinish(true);
+  m_pPrimaryTile->getGrid()->addChild(s, LayerOrder::PARTICLES);
+
+  /**
+   * Find direction of the match and rotate
+   * if necessary
+   */
+  auto it = getTileSet()->begin();
+  Tile* t1 = *it++;
+  Tile* t2 = *it;
+  if(t1->getPositionY() != t2->getPositionY()) {
+    p->setRotation(90);
+  }
   Match::run();
 }
