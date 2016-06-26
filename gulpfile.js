3819ede36a252414043419165567582850395060
Jarad DeLorenzo <jarad@splashthat.com>
2016-06-26 12:53:04 -0400

    Implemented a gulp pipeline for auto packing the sprite sheets. it's fucking awesome.

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,27 @@
 var gulp = require('gulp');
-var glue = require("gulp-glue");
+var shell = require('gulp-shell');
 var copy = require("gulp-copy");
 var rename = require("gulp-rename");
 
-gulp.task('sprites:pack', function() {
-    return gulp
-        .src(['/Users/delorenj/cc/Lorafel/sprites/raw/**/*.*'])
-        .pipe(glue({
-            'source': '/Users/delorenj/cc/Lorafel/sprites/raw',
-            'output': '/Users/delorenj/cc/Lorafel/sprites/sheets',                      // required
-            'project': true,
-            'retina': false,
-            'cocos2d': true
+gulp.task('sprites:pack', function () {
+    return gulp.src(['/Users/delorenj/cc/Lorafel/sprites/raw/*'], {read: false})
+        .pipe(shell([
+            'echo Packing sprites in <%= f(file.path) %>...',
+            'mkdir -p <%= file.path %>/../../sheets/<%= f(file.path) %>',
+            'spriteglue <%= file.path %> --sheet <%= file.path %>/../../sheets/<%= f(file.path) %>/sprites.png --powerOf2 --padding=0 --max-size-w=2048 --max-size-h=2048',
+        ], {
+            templateData: {
+                f: function(s) {
+                    var splits = s.split('/');
+                    return splits[splits.length-1];
+                }
+            }
         }))
-    ;
-});
+})
 
-gulp.task('sprites:copy', function() {
+gulp.task('sprites:copy',['sprites:pack'], function() {
    return gulp
-       .src('/Users/delorenj/cc/Lorafel/sprites/sheets/*')
-       .pipe(rename(function(path){
-           var name = path.basename;
-           path.dirname = name;
-           path.basename = "sprites";
-       }))
+       .src('/Users/delorenj/cc/Lorafel/sprites/sheets/**/sprites.*')
        .pipe(gulp.dest("/Users/delorenj/code/Lorafel/Resources"))
     ;
 });
