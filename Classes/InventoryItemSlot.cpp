5b7376fa7868c0e945aecff2308cfaf3e1a5cd92
Jarad DeLorenzo <jarad@splashthat.com>
2016-09-21 08:32:05 -0400

    Added item detail window asset. Implemented item detail window class. Implemented item slot touch init of detail window

--- Classes/InventoryItemSlot.cpp
+++ Classes/InventoryItemSlot.cpp
@@ -6,6 +6,7 @@
 #include "PlayerManager.h"
 #include "EventDataItem.h"
 #include "InventoryModal.h"
+#include "ItemDetailWindow.h"
 
 using namespace lorafel;
 
@@ -68,123 +69,134 @@ void InventoryItemSlot::addEvents() {
     };
 
     listener->onTouchEnded = [&](cocos2d::Touch* touch, cocos2d::Event* event) {
-        auto currentHoveredSlot = m_pGrid->getSlotFromPosition(m_pGrid->convertToNodeSpace(touch->getLocation()));
-        std::pair<int, int> chsCoords;
-        cocos2d::Sequence* seq;
-        Consumable* consumable = dynamic_cast<Consumable*>(m_pItem);
-
         /**
-         * Turn off selection highlights no matter what
+         * If touch ended and there was no touch move,
+         * the we will select the item and show item detail
+         * window instead of trying to swap the item position
+         * or try to equip it
          */
-        _eventDispatcher->dispatchCustomEvent("inventory-item-unselected");
-
-        if(currentHoveredSlot != nullptr) {
-            /**
-             * If over a slot, then set the destination
-             * to the hovered slot
-             */
-            auto chsCoords = currentHoveredSlot->getCoords();
-            auto scaleTo = cocos2d::ScaleTo::create(0.2f, m_originalScale);
-            auto to1 = currentHoveredSlot->getPosition();
-            auto speed1 = m_pGhost->getPosition().getDistance(to1)/800;
-            auto moveTo = cocos2d::MoveTo::create(speed1, convertToNodeSpace(m_pGrid->convertToWorldSpace(to1)));
-            auto s1 = cocos2d::Spawn::create(scaleTo, moveTo, nullptr);
-
-            /**
-             * Send the dragged item to the hovered slot,
-             * since you're def over a hovered slot
-             */
-            auto callback = cocos2d::CallFuncN::create([=](cocos2d::Node* sender) {
-                ghostOff();
-                m_pGrid->swap(this->getCoords(), chsCoords);
-            });
-
-            seq = cocos2d::Sequence::create(s1, callback, nullptr);
-
+        if(m_state == State::TOUCH_BEGIN) {
+            ItemDetailWindow* itemDetailWindow = ItemDetailWindow::create(this);
+            addChild(itemDetailWindow);
         } else {
+            auto currentHoveredSlot = m_pGrid->getSlotFromPosition(m_pGrid->convertToNodeSpace(touch->getLocation()));
+            std::pair<int, int> chsCoords;
+            cocos2d::Sequence* seq;
+            Consumable* consumable = dynamic_cast<Consumable*>(m_pItem);
+
             /**
-             * If over a valid equip slot, then equip
-             * the item
-             */
-            auto equipSlot = m_pGrid->getEquipSlotFromPosition(convertToWorldSpace(m_pGhost->getPosition()));
-            if(equipSlot != nullptr) {
+			 * Turn off selection highlights no matter what
+			 */
+            _eventDispatcher->dispatchCustomEvent("inventory-item-unselected");
+
+            if(currentHoveredSlot != nullptr) {
                 /**
-                 * This means we're over an equip slot
-                 * We have to make sure we can equip
-                 * it here though
-                 */
-                if(m_pItem->canEquip(equipSlot->getEquipMask())) {
-                    auto im = static_cast<InventoryModal*>(m_pGrid->getParent());
-                    cocos2d::Node* n = im->getEquipGrid();
-                    auto scaleTo = cocos2d::ScaleTo::create(0.2f, m_originalScale);
-                    auto to1 = equipSlot->getPosition();
-                    auto speed1 = m_pGhost->getPosition().getDistance(to1)/800;
-                    auto moveTo = cocos2d::MoveTo::create(speed1, convertToNodeSpace(n->convertToWorldSpace(to1)));
-                    auto s1 = cocos2d::Spawn::create(scaleTo, moveTo, nullptr);
+				 * If over a slot, then set the destination
+				 * to the hovered slot
+				 */
+                auto chsCoords = currentHoveredSlot->getCoords();
+                auto scaleTo = cocos2d::ScaleTo::create(0.2f, m_originalScale);
+                auto to1 = currentHoveredSlot->getPosition();
+                auto speed1 = m_pGhost->getPosition().getDistance(to1)/800;
+                auto moveTo = cocos2d::MoveTo::create(speed1, convertToNodeSpace(m_pGrid->convertToWorldSpace(to1)));
+                auto s1 = cocos2d::Spawn::create(scaleTo, moveTo, nullptr);
 
+                /**
+				 * Send the dragged item to the hovered slot,
+				 * since you're def over a hovered slot
+				 */
+                auto callback = cocos2d::CallFuncN::create([=](cocos2d::Node* sender) {
+                    ghostOff();
+                    m_pGrid->swap(this->getCoords(), chsCoords);
+                });
+
+                seq = cocos2d::Sequence::create(s1, callback, nullptr);
+
+            } else {
+                /**
+				 * If over a valid equip slot, then equip
+				 * the item
+				 */
+                auto equipSlot = m_pGrid->getEquipSlotFromPosition(convertToWorldSpace(m_pGhost->getPosition()));
+                if(equipSlot != nullptr) {
                     /**
-                     * Send the dragged item to the equip slot,
-                     */
-                    auto callback = cocos2d::CallFuncN::create([=](cocos2d::Node* sender) {
-                        /**
-                         * Now, turn off the ghost image and replace with the
-                         * image rendered by equip slot. Also, equip the slot.
-                         */
-                        ghostOff();
+					 * This means we're over an equip slot
+					 * We have to make sure we can equip
+					 * it here though
+					 */
+                    if(m_pItem->canEquip(equipSlot->getEquipMask())) {
+                        auto im = static_cast<InventoryModal*>(m_pGrid->getParent());
+                        cocos2d::Node* n = im->getEquipGrid();
+                        auto scaleTo = cocos2d::ScaleTo::create(0.2f, m_originalScale);
+                        auto to1 = equipSlot->getPosition();
+                        auto speed1 = m_pGhost->getPosition().getDistance(to1)/800;
+                        auto moveTo = cocos2d::MoveTo::create(speed1, convertToNodeSpace(n->convertToWorldSpace(to1)));
+                        auto s1 = cocos2d::Spawn::create(scaleTo, moveTo, nullptr);
 
                         /**
-                         * If slot had something in it, put it
-                         * back in the item grid. When modal opens
-                         * again, this will happen automatically,
-                         * but need to do it no manually, 'cause.
-                         * Don't put it back though if it's a
-                         * consumable, 'cause.
-                         */
-                        if(equipSlot->getItem() != nullptr) {
-                            auto tmp = getItem();
-
-                            if(consumable == nullptr) {
-                                setItem(equipSlot->getItem(), equipSlot->getStackSize());
-                            }
-
-                            equipSlot->setItem(tmp);
-
-                        } else {
-                            equipSlot->setItem(m_pItem);
+						 * Send the dragged item to the equip slot,
+						 */
+                        auto callback = cocos2d::CallFuncN::create([=](cocos2d::Node* sender) {
+                            /**
+							 * Now, turn off the ghost image and replace with the
+							 * image rendered by equip slot. Also, equip the slot.
+							 */
+                            ghostOff();
 
                             /**
-                             * Only remove the item from the item grid
-                             * if the item is not stackable
-                             */
-                            if(consumable == nullptr) {
-                                setItem(nullptr);
+							 * If slot had something in it, put it
+							 * back in the item grid. When modal opens
+							 * again, this will happen automatically,
+							 * but need to do it no manually, 'cause.
+							 * Don't put it back though if it's a
+							 * consumable, 'cause.
+							 */
+                            if(equipSlot->getItem() != nullptr) {
+                                auto tmp = getItem();
+
+                                if(consumable == nullptr) {
+                                    setItem(equipSlot->getItem(), equipSlot->getStackSize());
+                                }
+
+                                equipSlot->setItem(tmp);
+
+                            } else {
+                                equipSlot->setItem(m_pItem);
+
+                                /**
+								 * Only remove the item from the item grid
+								 * if the item is not stackable
+								 */
+                                if(consumable == nullptr) {
+                                    setItem(nullptr);
+                                }
                             }
-                        }
-                    });
+                        });
 
-                    seq = cocos2d::Sequence::create(s1, callback, nullptr);
-                    m_pGhost->runAction(seq);
-                    return;
+                        seq = cocos2d::Sequence::create(s1, callback, nullptr);
+                        m_pGhost->runAction(seq);
+                        return;
 
+                    }
                 }
+                /**
+				 * If not over a slot, then return the item
+				 * back to it's origin slot
+				 */
+                auto scaleTo = cocos2d::ScaleTo::create(0.2f, m_originalScale);
+                auto speed = m_pGhost->getPosition().getDistance(cocos2d::Vec2::ZERO)/800;
+                auto moveTo = cocos2d::MoveTo::create(speed, cocos2d::Vec2(getContentSize().width/2,getContentSize().height/2));
+                auto s1 = cocos2d::Spawn::create(scaleTo, moveTo, nullptr);
+                auto callback = cocos2d::CallFuncN::create([=](cocos2d::Node* sender) {
+                    ghostOff();
+                });
+
+                seq = cocos2d::Sequence::create(s1, callback, nullptr);
             }
-            /**
-             * If not over a slot, then return the item
-             * back to it's origin slot
-             */
-            auto scaleTo = cocos2d::ScaleTo::create(0.2f, m_originalScale);
-            auto speed = m_pGhost->getPosition().getDistance(cocos2d::Vec2::ZERO)/800;
-            auto moveTo = cocos2d::MoveTo::create(speed, cocos2d::Vec2(getContentSize().width/2,getContentSize().height/2));
-            auto s1 = cocos2d::Spawn::create(scaleTo, moveTo, nullptr);
-            auto callback = cocos2d::CallFuncN::create([=](cocos2d::Node* sender) {
-                ghostOff();
-            });
 
-            seq = cocos2d::Sequence::create(s1, callback, nullptr);
+
+            m_pGhost->runAction(seq);
         }
-
-
-        m_pGhost->runAction(seq);
     };
 
     cocos2d::Director::getInstance()->getEventDispatcher()->addEventListenerWithSceneGraphPriority(listener, this);
