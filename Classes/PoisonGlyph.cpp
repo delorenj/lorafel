2b515ebec3520ef4d9ec12174c614ebc3a6f5e29
Jarad DeLorenzo <jaradd@gmail.com>
2017-04-14 08:02:12 -0400

    Implementing generic stat buff/debuff classes for timed continuous, discrete, and static stat losses and gains

--- Classes/PoisonGlyph.cpp
+++ Classes/PoisonGlyph.cpp
@@ -5,6 +5,7 @@
 #include "PoisonGlyph.h"
 #include "HeroTile.h"
 #include "Globals.h"
+#include "PlayerManager.h"
 
 using namespace lorafel;
 
@@ -12,6 +13,10 @@ void PoisonGlyph::onMatch(Match* pMatch) {
     auto hero = m_pSwappyGrid->getHeroTile();
     if(hero != nullptr) {
         static_cast<HeroTile*>(hero)->applyHit(pMatch);
+        TimedContinuousStatEffect statEffect = TimedContinuousStatEffect::create();
+        statEffect->setDurationInSeconds(5);
+        statEffect->setTotalStatDelta(PlayerManager::getInstance()->getPlayer()->getMaxHp() * -0.25f);
+        statEffect->statSetterCallback(//TODO: Write callback to set stat);
         Tile::onMatch(pMatch);
     }
 }
